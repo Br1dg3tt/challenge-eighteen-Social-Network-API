05fe70d8e559603a5ee941f1d57304828500e81d
Br1dg3tt <br1dg3tt3v3@gmail.com>
2024-11-24 22:15:02 -0700

    user models

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+
+const validateEmail = function(email) {
+    const response = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+    return response.test(email);
+}
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: 'true',
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: 'true',
+            unique: true,
+            trim: true,
+            lowercase: true,
+            validate: [validateEmail, 'Please enter a valid email address'],
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thoughts',
+            },
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+})
+
+const User = model('User', userSchema);
+
+module.exports = User;
